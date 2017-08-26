96fd781cccf8d1f6865fe642d8dacd46b554bce8
Tomasz Seidler <blobbas@gmail.com>
2017-08-26 15:41:30 +0200

    added buildings

--- src/com/zoo/infrastructure/Building.java
+++ src/com/zoo/infrastructure/Building.java
@@ -1,4 +1,38 @@
 package com.zoo.infrastructure;
 
+import com.zoo.animals.Animal;
+
+import java.util.ArrayList;
+
 public class Building {
+
+    private ArrayList<Animal> animals;
+
+    private String name;
+
+    public Building(String name) {
+        this.name = name;
+        animals = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+        System.out.println("Animal added to building: " + name);
+    }
+
+    public Integer getAnimalCount() {
+        return animals.size();
+    }
+
+    public String getAnimalTypes() {
+        String result = "";
+        for (Animal animal : animals) {
+            result = result + animal.getClass().getCanonicalName() + "\n";
+        }
+        return result;
+    }
 }
